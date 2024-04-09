ce40374cfea3aa7c2624daa51ddeaf84c44c5490
buithienphat <146930716+buithienphat@users.noreply.github.com>
2024-04-09 10:35:24 +0700

    add socket

--- backend/api/server.js
+++ backend/api/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello");
 });
 app.use("/api/auth", authRoutes);
